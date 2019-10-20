36eaf4e02867ccf7fbb66508df716f0834e2a42f
musicode <musicode@qq.com>
2019-10-20 21:15:36 +0800

    导入 TerserPlugin

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
